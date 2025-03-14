04e131af04333cc2b2b931f1c694e4565a83aea9
Nguyen Van Tien Hai <nvthai061105@gmail.com>
2025-03-14 21:02:03 +0700

    hainvt fix

--- fe/src/routers/privateRoutes.js
+++ fe/src/routers/privateRoutes.js
@@ -1,6 +1,5 @@
 import DefaultLayout from "../layouts/DefaultLayout/DefaultLayout";
 import { Route } from "react-router-dom";
-import Profile from "../pages/auth/Profile";
 import AuthMiddleware from "../middlewares/authMiddleware";
 import DashboardPage from "../pages/admin/DashboardPage";
 import CreateLodgingPage from "../pages/lodging/CreateLodgingPage";
